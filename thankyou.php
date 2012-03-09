c11154fcecd475186df22ded4568f6dd256cd311
unknown <Forrest@Forrest-PC.(none)>
2012-03-08 19:18:12 -0500

    full codebase

---
+++ thankyou.php
@@ -0,0 +1,53 @@
+<?
+	require_once("preheader.php");
+	include("header.php");
+?>
+	     <div>
+			 <div style="clear: both;"></div>
+
+				<div class="page-Leftcol col">
+					<div class="left-col-inner">
+						<h2>Thank You For Your Message</h2>
+
+						<h5>Your email was successfully sent.</h5>
+						<p>
+							Thanks for contacting us. We will get back to you very shortly!
+						</p>
+
+					</div> <!-- end inner div -->
+				</div>
+
+				<? include('sidebar.inc.php');?>
+				<div style="clear:both;"> </div>
+
+
+			<div class="sub-featuredBox action-panels-leftcol first">
+				<img src="images/start-icon.png" class="icon" />
+				<div class="action-intro">
+					<h2>Start your Project Today</h2>
+						<p>
+							Lorem ipsum dolor sit amet, adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua, quis
+						</p>
+					<a href="#" class="cta start-project" title="Submit Your Project">Submit Your Project</a>
+					<div class="no-account">
+					Don't have an account?<br/>
+						<a href="#">Sign up</a> it's easy and free!
+					</div>
+				</div>
+			</div>
+			<div class="sub-featuredBox action-panels-leftcol">
+
+				<img src="images/browse-icon.png" class="icon" />
+				<div class="action-intro">
+					<h2>Browse Current Projects</h2>
+						<p>
+							Lorem ipsum dolor sit amet, adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+						</p>
+					<a href="#" class="cta browse-projects" title="Submit Your Project">Browse projects</a>
+				</div>
+			 </div>
+		</div>
+
+	<?
+	include("footer.php");
+?>
